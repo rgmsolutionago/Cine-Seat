d3d92744f375294dcdeb0675fb8c391df82eb85d
MichaelX17 <mfpersonal777@gmail.com>
2024-12-09 13:22:14 -0400

    Nuevos componentes y rutas configuradas

---
+++ src/app/seats/seats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seats',
+  standalone: true,
+  imports: [],
+  templateUrl: './seats.component.html',
+  styleUrl: './seats.component.css'
+})
+export class SeatsComponent {
+
+}
