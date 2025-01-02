85e7ce941239cb57972c8b51c48010c5bbb6e5b4
MichaelX17 <mfpersonal777@gmail.com>
2025-01-02 13:01:13 -0400

    Avances Pantalla Seats

--- src/app/view/seats/seats.component.ts
+++ src/app/view/seats/seats.component.ts
@@ -1,28 +1,27 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { SoapClientService } from '../../core/services/soap-client.service';
-import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { SoapClientService } from "../../core/services/soap-client.service";
+import { CommonModule } from "@angular/common";
 
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
 
-import { SidebarMenuComponent } from '../../shared/sidebar-menu/sidebar-menu.component';
-import { LoadingComponent } from '../../shared/loading/loading.component';
+import { SidebarMenuComponent } from "../../shared/sidebar-menu/sidebar-menu.component";
+import { LoadingComponent } from "../../shared/loading/loading.component";
 
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-seats',
+  selector: "app-seats",
   standalone: true,
   imports: [CommonModule, SidebarMenuComponent, LoadingComponent],
-  templateUrl: './seats.component.html',
-  styleUrl: './seats.component.css'
+  templateUrl: "./seats.component.html",
+  styleUrl: "./seats.component.css",
 })
 export class SeatsComponent extends LoadingComponent {
-
-  @ViewChild('seatContainer') seatContainer!: ElementRef;
+  @ViewChild("seatContainer") seatContainer!: ElementRef;
 
   seatsPrint: any = [];
 
-  seatImage: string = '';
+  seatImage: string = "";
 
   maxX: number = 0;
 
@@ -51,34 +50,30 @@ export class SeatsComponent extends LoadingComponent {
   ) {
     super();
   }
-  
+
   async ngOnInit() {
-
-
     const sessionValid = await this.ValSession();
 
     if (!sessionValid) {
-      this.router.navigate(['/login']);
+      this.router.navigate(["/login"]);
       return;
     }
 
     this.openModal();
 
-    this.route.paramMap.subscribe(params => {
-      this.MovieId = params.get('movie');
+    this.route.paramMap.subscribe((params) => {
+      this.MovieId = params.get("movie");
       console.log(this.MovieId);
-      this.ScheduleId = params.get('schedule');
+      this.ScheduleId = params.get("schedule");
       console.log(this.ScheduleId);
-      this.ScheduleDate = params.get('date');
+      this.ScheduleDate = params.get("date");
     });
 
     await this.DataPeli();
     // await this.DataSeats();
-    
   }
 
-  async DataPeli(){
-
+  async DataPeli() {
     const movie = await this.soapClient.getMovies();
 
     console.log("movie", movie);
@@ -86,45 +81,43 @@ export class SeatsComponent extends LoadingComponent {
     var peliculas: any = [];
 
     if (Array.isArray(movie)) {
-
       peliculas = movie;
-
     } else if (peliculas) {
-
       peliculas.push(movie);
     }
-    
+
     // var peliculas = await this.soapClient.getMovies();
 
-    const movieFound = peliculas.some((peli: any) => peli.FeatureId === this.MovieId);
+    const movieFound = peliculas.some(
+      (peli: any) => peli.FeatureId === this.MovieId
+    );
 
     if (!movieFound) {
-
-      setTimeout(()=>{
+      setTimeout(() => {
         this.closeModal();
       }, 200);
-      
-      setTimeout(()=>{
-        this.router.navigate(['/home']);
+
+      setTimeout(() => {
+        this.router.navigate(["/home"]);
       }, 300);
-      
+
       return;
     }
 
     const promises = peliculas.map(async (peli: any) => {
-
       if (peli.FeatureId === this.MovieId) {
-
         const poster = await this.soapClient.getPoster(peli.FeatureId);
 
         console.log("this.ScheduleDate", this.ScheduleDate);
 
-        const show = await this.soapClient.getShows(peli.FeatureId, this.ScheduleDate);
+        const show = await this.soapClient.getShows(
+          peli.FeatureId,
+          this.ScheduleDate
+        );
 
-        console.log("show", show)
-        
+        console.log("show", show);
+
         if (show.length > 0) {
-
           const show_new = await this.dataShowSort(show, peli.TotalRuntime);
 
           const data = {
@@ -133,21 +126,21 @@ export class SeatsComponent extends LoadingComponent {
             Title: peli.Title,
             TotalRuntime: peli.TotalRuntime,
             Poster: poster,
-            Show: show_new
+            Show: show_new,
           };
 
           this.peliAcutal = data; // Agrega los datos al array
-          console.log(this.peliAcutal)
+          console.log(this.peliAcutal);
         }
       }
     });
-    
+
     await Promise.all(promises);
 
     this.filterShows();
     await this.DataSeats();
 
-    setInterval(async() => {
+    setInterval(async () => {
       this.now = new Date();
       await this.filterShows();
       await this.DataSeats();
@@ -155,12 +148,9 @@ export class SeatsComponent extends LoadingComponent {
 
     console.log("peliActual", this.peliAcutal);
     console.log("showActual", this.showActual);
-
-    
   }
 
   filterShows() {
-
     const movie = this.peliAcutal;
 
     const totalMinutes = parseInt(movie.TotalRuntime, 10);
@@ -168,36 +158,37 @@ export class SeatsComponent extends LoadingComponent {
     let relevantShow: any = {};
 
     if (movie) {
-
       relevantShow = movie.Show.find((show: any) => {
         const startTime = new Date(show.StartDate);
         const endTime = new Date(show.EndDate);
         const endThreshold = new Date(endTime.getTime() - 20 * 60 * 1000); // EndDate - 20 minutos
-  
+
         // Calcular la diferencia en minutos
-        const diffInMinutes = Math.floor((this.now.getTime() - startTime.getTime()) / 60000);
+        const diffInMinutes = Math.floor(
+          (this.now.getTime() - startTime.getTime()) / 60000
+        );
         const adjustedDiff = totalMinutes - diffInMinutes;
-        const starts = Math.floor((startTime.getTime() - this.now.getTime()) / 60000);
-        
-        show.Elapsed = this.convertMinutesAndHours(diffInMinutes),
-        show.Remaining = this.convertMinutesAndHours(adjustedDiff),
-        show.TotalMinutes = this.convertMinutesAndHours(totalMinutes)
+        const starts = Math.floor(
+          (startTime.getTime() - this.now.getTime()) / 60000
+        );
+
+        (show.Elapsed = this.convertMinutesAndHours(diffInMinutes)),
+          (show.Remaining = this.convertMinutesAndHours(adjustedDiff)),
+          (show.TotalMinutes = this.convertMinutesAndHours(totalMinutes));
         show.Progress = this.calculateProgress(startTime, endTime);
         show.Starts = this.convertMinutesAndHours(starts);
-  
+
         return (
           (this.now >= startTime && this.now < endThreshold) || // Ya comenzó
           (this.now >= endThreshold && this.now <= endTime) || // Terminando
-          (this.now < startTime) // Por empezar
+          this.now < startTime // Por empezar
         );
       });
-
     }
 
-    console.log(relevantShow)
+    console.log(relevantShow);
 
     if (relevantShow) {
-
       const startTime = new Date(relevantShow.StartDate);
       const endTime = new Date(relevantShow.EndDate);
       const endThreshold = new Date(endTime.getTime() - 20 * 60 * 1000); // EndDate - 20 minutos
@@ -215,7 +206,7 @@ export class SeatsComponent extends LoadingComponent {
         relevantShow.StatusPeli = 3;
       }
       // Devolver solo una película con un show relevante
-      this.showActual =  relevantShow;
+      this.showActual = relevantShow;
       // console.log(this.showActual);
     }
   }
@@ -228,37 +219,37 @@ export class SeatsComponent extends LoadingComponent {
   }
 
   calculateProgress(startDate: Date, endDate: Date): number {
-    
     const totalDuration = endDate.getTime() - startDate.getTime();
     const elapsed = this.now.getTime() - startDate.getTime();
     return (elapsed / totalDuration) * 100;
   }
 
-  async dataShowSort(shows: any[], timeTotal:string){
-
+  async dataShowSort(shows: any[], timeTotal: string) {
     const totalMinutes = parseInt(timeTotal, 10);
 
     shows.sort((a, b) => a.StartTime.localeCompare(b.StartTime));
 
-    return shows.map(show => {
-
-      const startDate = new Date(`${show.ScheduleDate.slice(0, 4)}-${show.ScheduleDate.slice(4, 6)}-${show.ScheduleDate.slice(6, 8)}T${show.StartTime}`);
+    return shows.map((show) => {
+      const startDate = new Date(
+        `${show.ScheduleDate.slice(0, 4)}-${show.ScheduleDate.slice(
+          4,
+          6
+        )}-${show.ScheduleDate.slice(6, 8)}T${show.StartTime}`
+      );
       const endDate = new Date(startDate.getTime() + totalMinutes * 60000);
-      
+
       return {
         StartTime: show.StartTime,
         ScreenName: show.ScreenName,
         ScheduleDate: show.ScheduleDate,
         ScheduleId: show.ScheduleId,
         StartDate: startDate,
-        EndDate: endDate
+        EndDate: endDate,
       };
-
     });
   }
 
-  async DataSeats(){
-
+  async DataSeats() {
     const session = await this.soapClient.getSession();
 
     var seats = await this.soapClient.getSeats(session, this.ScheduleId);
@@ -267,12 +258,15 @@ export class SeatsComponent extends LoadingComponent {
 
     const max = await this.findMaxCoordinates(seats);
 
-    seats = await this.subtractMinCoordinates(seats, coordinates.minX, coordinates.minY);
+    seats = await this.subtractMinCoordinates(
+      seats,
+      coordinates.minX,
+      coordinates.minY
+    );
 
     await this.findMaxCoordinates(seats);
 
-    seats.sort((a:any, b:any) => {
-
+    seats.sort((a: any, b: any) => {
       const xA = parseInt(a.X, 10);
       const yA = parseInt(a.Y, 10);
       const xB = parseInt(b.X, 10);
@@ -280,11 +274,9 @@ export class SeatsComponent extends LoadingComponent {
 
       if (yA !== yB) {
         return yA - yB;
-      }
-      else {
+      } else {
         return xA - xB;
       }
-
     });
 
     this.letters = await this.extractUniqueLetters(seats, coordinates.minX);
@@ -292,32 +284,29 @@ export class SeatsComponent extends LoadingComponent {
     this.seatsPrint = seats;
 
     // console.log(this.seatsPrint);
-    setTimeout(()=>{
+    setTimeout(() => {
       this.captureSeatsAsImage();
     }, 100);
-    
-    
+
     // console.log(seats);
 
     // console.log(this.letters);
   }
 
-  async findMinCoordinates(seats: any[]){
-
+  async findMinCoordinates(seats: any[]) {
     let minX = Infinity;
     let minY = Infinity;
 
-    seats.forEach(seat => {
-
+    seats.forEach((seat) => {
       const x = parseInt(seat.X, 10);
       const y = parseInt(seat.Y, 10);
 
       if (x < minX) {
         minX = x;
-      } if (y < minY) {
+      }
+      if (y < minY) {
         minY = y;
       }
-
     });
 
     // Se ajusta minimo de X para agregar las letras
@@ -329,29 +318,28 @@ export class SeatsComponent extends LoadingComponent {
   async findMaxCoordinates(seats: any[]) {
     let maxX = -Infinity;
     let maxY = -Infinity;
-  
-    seats.forEach(seat => {
+
+    seats.forEach((seat) => {
       const x = parseInt(seat.X, 10);
       const y = parseInt(seat.Y, 10);
-  
+
       if (x > maxX) {
         maxX = x;
       }
-  
+
       if (y > maxY) {
         maxY = y;
       }
     });
-  
+
     this.maxX = maxX + 20;
     this.maxY = maxY + 26;
   }
-  
-  async extractUniqueLetters(seats: any[], minX: number) {
 
+  async extractUniqueLetters(seats: any[], minX: number) {
     const lettersSet = new Map<string, number>();
 
-    seats.forEach(seat => {
+    seats.forEach((seat) => {
       const letter = seat.SeatNumber.charAt(0);
       // Obtener la letra
       lettersSet.set(letter, seat.Y);
@@ -361,7 +349,7 @@ export class SeatsComponent extends LoadingComponent {
     const lettersArray = Array.from(lettersSet).map(([letter, y], index) => ({
       letter,
       X: 0, // Colocar la letra a la izquierda de los asientos
-      Y: y // Colocar la letra al comienzo de cada fila
+      Y: y, // Colocar la letra al comienzo de cada fila
     }));
 
     return lettersArray;
@@ -369,10 +357,10 @@ export class SeatsComponent extends LoadingComponent {
   }
 
   async subtractMinCoordinates(seats: any[], minX: number, minY: number) {
-    return seats.map(seat => ({
+    return seats.map((seat) => ({
       ...seat,
       X: (parseInt(seat.X, 10) - minX).toString(),
-      Y: (parseInt(seat.Y, 10) - minY).toString()
+      Y: (parseInt(seat.Y, 10) - minY).toString(),
     }));
   }
 
@@ -382,9 +370,9 @@ export class SeatsComponent extends LoadingComponent {
     // console.log(element);
 
     const canvas = await html2canvas(element, {
-      backgroundColor: null
+      backgroundColor: null,
     });
-    this.seatImage = canvas.toDataURL('image/png');
+    this.seatImage = canvas.toDataURL("image/png");
 
     // console.log(this.seatImage);
     this.closeModal();
@@ -392,21 +380,24 @@ export class SeatsComponent extends LoadingComponent {
 
   formatDateToString(date: Date): string {
     const year = date.getFullYear().toString();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
     return `${year}${month}${day}`;
   }
-  
-  async ValSession(){
-    const userSession = localStorage.getItem('userSession');
+
+  async ValSession() {
+    const userSession = localStorage.getItem("userSession");
 
     console.log("userSession", userSession?.trim());
 
-    if (!userSession || userSession.trim() == '') {
+    if (!userSession || userSession.trim() == "") {
       return false;
     }
 
     return true;
   }
 
+  goBack() {
+    this.router.navigate(["/home"]);
+  }
 }
