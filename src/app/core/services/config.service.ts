37244f379219a71feff3432197f9aeb68fe37f51
yerman2 <yermanratia2@gmail.com>
2025-01-11 20:22:06 -0400

    casi

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -6,19 +6,28 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class ConfigService {
-  private configUrl = 'assets/js/config.js';  // Ruta del archivo de configuración
+  private configUrl = 'assets/js/config.json';  // URL del archivo de configuración
   // private configUrl = 'cine-butacas/assets/js/config.js';
   private configSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
   constructor(private http: HttpClient) {}
 
   loadConfig(): void {
+    console.log('Intentando cargar el archivo de configuración desde:', this.configUrl); // Log para verificar la URL
+
     this.http.get<any>(this.configUrl).subscribe(
       (data) => {
+        console.log('Configuración cargada con éxito:', data);  // Log de los datos recibidos
         this.configSubject.next(data);  // Actualiza el valor de la configuración
       },
       (error) => {
-        console.error('Error al cargar la configuración:', error);
+        console.error('Error al cargar la configuración:', error);  // Log de error
+        // Agregar más detalles sobre el error si es posible
+        if (error.status) {
+          console.error('Código de error:', error.status);
+          console.error('Texto del error:', error.statusText);
+        }
+        console.error('Detalles del error:', error);
       }
     );
   }
